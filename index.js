9669818d1f4cb49d1d377a1de0ffd9936d346689
Rayyan <fastray10@gmail.com>
2024-11-24 12:55:18 +0700

    Error handling for default and undefined path

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import router from "./routes/index.js";
+import { STATUS_CODE } from "./config/statusCode.js";
+import { createResponse } from "./utils/response.js";
 import { errorHandler } from "./middleware/errorMiddleware.js"
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => res.status(200).json(createResponse(STATUS_CODE.SUCCESS, "Web Service")))
+
 app.use(express.static('public'))
 app.use('/uploads', express.static('uploads'));
-  
-app.use(express.json());
+
 app.use(router);
+
+app.use((req, res, next) => res.status(404).json(createResponse(404, "URL Not Found")));
 
 // Generic error handler
 app.use(errorHandler)
